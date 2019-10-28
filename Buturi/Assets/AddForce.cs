a87ebe5386cec055b1614102a1f432e3c74d788b
Cashimom <hiroshi2000yoshida@gmail.com>
2019-10-28 10:15:55 +0900

    等速円運動
    等速円運動のsceneを作りました

---
+++ Buturi/Assets/AddForce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AddForce : MonoBehaviour
+{
+
+    [SerializeField] public Vector3 force = new Vector3(0, 0, 1);
+
+    [SerializeField] public ForceMode forceMode = ForceMode.Impulse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(force, forceMode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
